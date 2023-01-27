9ab5a2106c99865338bbf84d54e2addaa70f3bf9
nishigaki <nishigaki@aivick.co.jp>
2023-01-27 12:39:10 +0900

    Fix

--- pages/top/index.tsx
+++ pages/top/index.tsx
@@ -27,18 +27,18 @@ export default function Top(props: Props) {
 
 export async function getServerSideProps (context: any) {
     const generateAccessToken = async() => {
-        console.log(context.query);
     
-        const res = await axios.post(
-            process.env.NEXT_PUBLIC_LINE_ACCESS_TOKEN_URL || "",
-            {
-                grant_type: 'authorization_code',
-                code: context.query.code,
-                redirect_uri: process.env.NEXT_PUBLIC_LINE_AUTH_REDIRECT_URI,
-                client_id: process.env.NEXT_PUBLIC_LINE_AUTH_CLIENT_ID,
-                client_secret: process.env.NEXT_PUBLIC_LINE_AUTH_CLIENT_SECRET,
-            }
-        );
+        // const res = await axios.post(
+        //     process.env.NEXT_PUBLIC_LINE_ACCESS_TOKEN_URL || "",
+        //     {
+        //         grant_type: 'authorization_code',
+        //         code: context.query.code,
+        //         redirect_uri: process.env.NEXT_PUBLIC_LINE_AUTH_REDIRECT_URI,
+        //         client_id: process.env.NEXT_PUBLIC_LINE_AUTH_CLIENT_ID,
+        //         client_secret: process.env.NEXT_PUBLIC_LINE_AUTH_CLIENT_SECRET,
+        //     }
+        // );
+        const res = {data: "hoge"}
         
         return res.data
     }
