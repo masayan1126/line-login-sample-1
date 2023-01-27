577f805e41042fc8f6bf44e027d8c16727671cfe
nishigaki <nishigaki@aivick.co.jp>
2023-01-27 11:36:33 +0900

    Fix

--- pages/top/index.tsx
+++ pages/top/index.tsx
@@ -8,6 +8,22 @@ export default function Top() {
     const router = useRouter();
     useEffect(() => {
         console.log(router.query); // router.query.code, router.query.state
+        const generateAccessToken = async() => {
+            const res = await axios.post(
+                process.env.NEXT_PUBLIC_LINE_ACCESS_TOKEN_URL || "",
+                {
+                    grant_type: 'authorization_code',
+                    code: router.query.code,
+                    redirect_uri: process.env.NEXT_PUBLIC_LINE_AUTH_REDIRECT_URI,
+                    client_id: process.env.NEXT_PUBLIC_LINE_AUTH_CLIENT_ID,
+                    client_secret: process.env.NEXT_PUBLIC_LINE_AUTH_CLIENT_SECRET,
+                }
+            );
+    
+            console.log(res.data);
+        }
+
+        generateAccessToken()
     }, [router.query]);
 
   return (
