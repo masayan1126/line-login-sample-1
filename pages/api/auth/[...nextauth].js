a56aa56f60fe6718f95e7cbcd3d9d8a996572572
nishigaki <nishigaki@aivick.co.jp>
2023-01-28 13:47:21 +0900

    Fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,5 +8,31 @@ export default NextAuth({
       clientId: process.env.NEXT_PUBLIC_LINE_AUTH_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_LINE_AUTH_CLIENT_SECRET
     })
-  ]
+  ],
+    callbacks: {
+        // async jwt({ token, user, account }) {
+        //     if (account && user) {
+        //         return {
+        //         ...token,
+        //         accessToken: user.token,
+        //         refreshToken: user.refreshToken,
+        //         };
+        //     }
+        
+        //     return token;
+        // },
+        async jwt({ token, account }) {
+            if (account) {
+              token.accessToken = account.access_token
+            //   token.userId = account.userId
+            //   token.idToken = account.id_token
+            }
+            return token
+        },
+        async session({ session, token }) {
+            session.user.accessToken = token.accessToken;
+            session.user.userId = token.userId;
+            return session;
+        },
+    },
 })
---
+++ pages/sample/index.tsx
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from 'react';
+
+const Sample = () => {
+
+    const [rangeValue, setRangeValue] = useState<number>(0)
+    const [inputValue, setInputValue] = useState<string>("")
+    const [text, setText] = useState<string>("")
+
+    // const handleChange = (e: React.ChangeEvent<HTMLInputElement> ) => {
+    //     setRangeValue(e.target.valueAsNumber)
+        
+    // }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement> ) => {
+        setInputValue(e.target.value)
+        
+    }
+
+    // useEffect(() => {
+    //     if (rangeValue === 0) {
+    //         return
+    //     }
+
+    //     setTimeout(() => {
+    //         setText("ページ遷移します")
+            
+    //     }, 2000);
+    
+    // }, [rangeValue])
+
+    useEffect(() => {
+        if (inputValue === "") {
+            return
+        }
+
+        setTimeout(() => {
+            setText("ページ遷移します")
+            
+        }, 2000);
+    
+    }, [inputValue])
+    
+
+    return (
+        <>
+            <div>
+                <h3>体重</h3>
+                {/* <span>{inputValue} </span> */}
+                <p>{text}</p>
+                {/* <input type="range" value={rangeValue} onChange={handleChange} min="0" max="100" /> */}
+                <input type="text" value={inputValue} onChange={handleChange} min="0" max="100" />kg
+            </div>
+        </>
+    );
+}
+
+export default Sample
